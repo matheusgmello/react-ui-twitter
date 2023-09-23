0e49f2d09f35b3fb4d92bb17108f5912262fcb9f
Matheus Gabriel <matheusgmello1@gmail.com>
2023-09-22 23:43:11 -0300

    Feat: Header component

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Sparkle } from 'phosphor-react'
 import './global.css'
 
 import { Tweet } from './components/Tweet'
 import { Sidebar } from './components/Sidebar'
+import { Header } from './components/Header'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -12,10 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <Sidebar/>
       <div className="content">
         <main className="timeline">
-          <div className="timeline-header">
-            Home
-            <Sparkle/>
-          </div>
+         <Header title="Home"/>
 
           <form className="new-tweet-form">
             <label htmlFor="tweet">
